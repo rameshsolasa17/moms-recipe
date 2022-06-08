784c4b19cf3b23197cd3a7e2b156ba84c724cdb0
rameshsolasa <rameshsolasa17@gmail.com>
2022-06-08 20:38:34 +0530

    back commit

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use('/api/v1/recipes', recipeRoutes)
 //     res.json(recipe)
 // })
 
-const PORT = process.env.PORT || 8000 
+const PORT = process.env.PORT || 5000 
 app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 })
